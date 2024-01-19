bbd54b67ef8e4c3c5895d80d4a8b5f458d66ba1d
Camdyn Zook <camdynzook@gmail.com>
2024-01-19 14:36:14 -0500

    git commit -m "Add bash script to archive website directory"

---
+++ bash/backup_and_archive_website.sh
@@ -0,0 +1,14 @@
+```
+#!/bin/bash
+
+# Define website directory
+WEBSITE_DIR=/var/www/html
+
+# Define backup directory
+BACKUP_DIR=/backup
+
+# Create directory if it doesn't exist
+mkdir -p $BACKUP_DIR
+
+# Archive the website directory
+tar -cvzf $BACKUP_DIR/website_$(date +"%Y%m%d").tar.gz $WEBSITE_DIR
