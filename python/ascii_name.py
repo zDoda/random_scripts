13824a56893b2d86c2f0fbc05bdcd1fae4655fb0
Camdyn Zook <camdynzook@gmail.com>
2024-01-25 21:56:43 -0500

    text

--- python/ascii_name.py
+++ python/ascii_name.py
@@ -2,10 +2,10 @@ import pyfiglet
 
 
 def ascii_art(name):
-    ascii_name = pyfiglet.figlet_format(name, font='small_slant')
+    ascii_name = pyfiglet.figlet_format(name, font='ogre')
     return ascii_name
 
 
 # Replace 'YourName' with your actual name
-print(ascii_art("Camdyn Zook"))
+print(ascii_art("Julia Hornung"))
 # print(pyfiglet.FigletFont.getFonts())
